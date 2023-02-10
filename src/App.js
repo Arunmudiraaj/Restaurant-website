eebdb4c14b68efc12c08af982d4b3de563e40a6f
arun mudiraj <arunmudiraj18@gmail.com>
2023-02-10 13:06:38 +0530

    header completed

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from "./components/layout/Header";
+import React from "react";
+function App() {
+  return (
+    <React.Fragment>
+      <Header/>
+      </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,18 @@
+import './header.css'
+import mealsImage from '../../assets/meals.jpg'
+import HeaderCartButton from './HeaderCartButton'
+import { Fragment } from 'react'
+const Header = ()=>{
+    return <Fragment>
+        <header className='header'>
+            <h1>ReactMeals</h1>
+            <HeaderCartButton/>
+        </header>
+        <div className='main-image'>
+            <img src={mealsImage} alt='A  table of delicious food'/>
+        </div>
+       
+    </Fragment>
+}
+
+export default Header
