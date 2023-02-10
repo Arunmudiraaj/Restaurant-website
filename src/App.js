b3e4444ff6609dffb628609d16e6a69c512a9047
arun mudiraj <arunmudiraj18@gmail.com>
2023-02-10 13:58:55 +0530

    summary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./components/layout/Header";
 import React from "react";
+import Meals from "./components/meals/Meals";
 function App() {
   return (
     <React.Fragment>
       <Header/>
+      <main>
+        <Meals/>
+      </main>
       </React.Fragment>
   );
 }
